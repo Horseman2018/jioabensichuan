c677b37228997ead51dd04b60880e5ca5669eeab
tower <903741584@qq.com>
2024-03-24 13:01:51 +0800

    脚本测试的更改

--- 2.js
+++ 2.js
@@ -1,1 +1,16 @@
-console.log(hello world)
+// ==UserScript==
+// @name         测试脚本
+// @namespace    903741584@qq.com
+// @version      2024-03-24
+// @description  这是一个测试脚本
+// @author       tower
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('你好,这是测试脚本')
+})();
